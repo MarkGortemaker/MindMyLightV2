bfdc27bfa55072882109fe2b6ad7f55b7135d93e
Yusuf Abaci <s1208618@student.windesheim.nl>
2024-03-22 13:37:21 +0100

    add spawning, particles, animation and old balloon models

--- Assets/Scripts/Level 2/UpdateHUD.cs
+++ Assets/Scripts/Level 2/UpdateHUD.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class UpdateUI : MonoBehaviour
+public class UpdateHUD : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
 
