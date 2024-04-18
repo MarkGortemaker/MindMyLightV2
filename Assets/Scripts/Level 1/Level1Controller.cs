fbc3cf567a3a9f12761eb8ffb0e5aa7b04fb4f18
Yusuf Abaci <s1208618@student.windesheim.nl>
2024-04-18 12:49:01 +0200

    implement player movement and camera following

---
+++ Assets/Scripts/Level 1/Level1Controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Controller : MonoBehaviour
+{
+    public static float borderDistance = 10f;
+
+    public static List<GameObject> meteors = new List<GameObject>();
+    public static List<GameObject> comets = new List<GameObject>();
+    public static List<GameObject> stardusts = new List<GameObject>();
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
