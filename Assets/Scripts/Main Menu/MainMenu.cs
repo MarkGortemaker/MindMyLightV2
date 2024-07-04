8fb9cf1b2ab0c5fff100797776cc3adb511d950a
Mark <70703393+MarkGortemaker@users.noreply.github.com>
2024-07-04 16:47:03 +0200

    Dialogue Localization Fix

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -1,31 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Start()
+    {
+        if (!PlayerPrefs.HasKey("JSONDir"))
+        {
+            PlayerPrefs.SetString("JSONDir", "DiaTextSystem\\ENG");
+        }
+        //set default localization to English
+    }
+
     public void Quit()
     {
         Application.Quit();
-
-
     }
 
     public void SetLocalization(int localization)
     {
         switch (localization)
         {
-            case 0: PlayerPrefs.SetString("JSONDir", "DiaTextSystem\\ENG");
+            case 0:
+                PlayerPrefs.SetString("JSONDir", "DiaTextSystem\\ENG");
                 break;
-            case 1: PlayerPrefs.SetString("JSONDir", "DiaTextSystem\\NL");
+            case 1:
+                PlayerPrefs.SetString("JSONDir", "DiaTextSystem\\NL");
                 break;
-            case 2: PlayerPrefs.SetString("JSONDir", "DiaTextSystem\\UKR");
+            case 2:
+                PlayerPrefs.SetString("JSONDir", "DiaTextSystem\\UKR");
                 break;
-            default: Debug.LogWarning("Unknown localization.");
+            default:
+                Debug.LogWarning("Unknown localization.");
                 break;
         }
         Debug.Log(PlayerPrefs.GetString("JSONDir"));
     }
-
 }
